3127226457dfd919c632ce43537d396af2f2281f
Farid Azhar <crackgods777@gmail.com>
2023-01-19 19:42:48 +0700

    Fortify
    - Auth CSS

--- resources/views/public/homepage.blade.php
+++ resources/views/public/homepage.blade.php
@@ -1,8 +1,38 @@
 @extends('template.global.page')
 
 @section('main')
-    <section class="grid grid-cols-1 md:grid-cols-3 gap-5">
-        <div class="col-span-1 md:col-span-2">Content</div>
-        <div class="col-span-1">Sidebar</div>
+    <section class="grid grid-cols-1 md:grid-cols-4 gap-5">
+        <div class="col-span-1 md:col-span-3">
+            <section>Content</section>
+            <section>Content</section>
+            <section>Content</section>
+        </div>
+        <div class="col-span-1">
+            <section class="sidebar-announcements">
+                <div class="mb-2 text-lg font-semibold uppercase tracking-wide">Announcements</div>
+                <div class="bg-white shadow mx-auto p-5 rounded-md border border-gray-300">
+                    <div></div>
+                </div>
+            </section>
+            <section class="sidebar-events">
+                <div class="mb-2 text-lg font-semibold uppercase tracking-wide">Events</div>
+                <div class="bg-white shadow mx-auto p-5 rounded-md border border-gray-300">
+                    <div></div>
+                </div>
+            </section>
+            <section class="sidebar-stats">
+                <div class="mb-2 text-lg font-semibold uppercase tracking-wide">Stats</div>
+                <div class="bg-white shadow mx-auto p-5 rounded-md border border-gray-300 text-sm space-y-3">
+                    <div class="flex items-center justify-between">
+                        <div><i class="fas fa-users fa-fw mr-2"></i>Users</div>
+                        <div class="bg-palette-4 text-palette-1 py-0.5 px-2 rounded">{{ number_format($users->value) }}</div>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div><i class="fas fa-mask fa-fw mr-2"></i>Cosplayers</div>
+                        <div class="bg-palette-4 text-palette-1 py-0.5 px-2 rounded">{{ number_format($cosplayers->value ?? 0) }}</div>
+                    </div>
+                </div>
+            </section>
+        </div>
     </section>
 @endsection
--- resources/views/template/global/header.blade.php
+++ resources/views/template/global/header.blade.php
@@ -1,26 +1,32 @@
 <header class="sticky" x-data="{ menuOpen: false }">
     <div class="header">
-        <div class="sm:hidden">
+        <div class="mobile-menu-btn">
             <button class="btn btn-tertiary" @click="menuOpen = !menuOpen" @click.outside="menuOpen = false">
                 <i class="fas fa-bars" x-show="!menuOpen"></i>
                 <i class="fas fa-bars-staggered" x-show="menuOpen"></i>
             </button>
         </div>
-        <div class="flex items-center">
+        <div class="navbar">
             <div class="text-xl font-medium tracking-wide">
                 <a href="{{ route('homepage') }}">{{ config('app.name', 'Cosplayer.gg') }}</a>
             </div>
-            <div class="hidden sm:flex items-center ml-10 space-x-5 text-gray-900">
-                <div><a href="#">Home</a></div>
-                <div><a href="#">Home</a></div>
-                <div><a href="#">Home</a></div>
-                <div><a href="#">Home</a></div>
+            <div class="navbar-links">
+                <div>
+                    <a href="#"><i class="fas fa-home fa-fw mr-1"></i>Home</a>
+                </div>
+                <div>
+                    <a href="#"><i class="fas fa-mask fa-fw mr-1"></i>Cosplay</a>
+                </div>
+                <div>
+                    <a href="#"><i class="fas fa-calendar fa-fw mr-1"></i>Event</a>
+                </div>
+                <div>
+                    <a href="#"><i class="fas fa-circle-xmark fa-fw mr-1"></i>Scammer</a>
+                </div>
                 <div x-data="{ dropdownOpen: false }">
-                    <a href="#" @click="dropdownOpen = ! dropdownOpen" @click.outside="dropdownOpen = false">More</a>
-                    <div class="absolute z-10 mt-2 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" x-show="dropdownOpen" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700">More Link</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700">More Link</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700">More Link</a>
+                    <a href="#more" @click="dropdownOpen = ! dropdownOpen" @click.outside="dropdownOpen = false"><i class="fas fa-angle-down fa-fw mr-1"></i>More</a>
+                    <div class="dropdown origin-top-left" x-show="dropdownOpen" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95">
+                        <a href="#"><i class="fab fa-discord fa-fw mr-1"></i>Discord</a>
                     </div>
                 </div>
             </div>
@@ -29,13 +35,16 @@
             <div class="relative ml-3" x-data="{ profileOpen: false }">
                 <div>
                     <button @click="profileOpen = !profileOpen" @click.outside="profileOpen = false" type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                        <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                        <img class="h-8 w-8 rounded-full" src="{{ asset('uploads/avatar/' . auth()->user()->avatar) }}" onerror="this.src='{{ asset('assets/avatar/default_avatar.png') }}'">
                     </button>
                 </div>
-                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" x-show="profileOpen" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700">Your Profile</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700">Settings</a>
-                    <a href="#logout" class="block px-4 py-2 text-sm text-gray-700" onclick="event.preventDefault(); document.getElementById('logout').submit();">Logout</a>
+                <div class="dropdown right-0 w-48 origin-top-right" x-show="profileOpen" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95">
+                    <a href="#">Profile</a>
+                    <a href="#">Settings</a>
+                    @can('access-filament')
+                        <a href="{{ config('filament.path') }}">Admin Panel</a>
+                    @endcan
+                    <a href="#logout" onclick="event.preventDefault(); document.getElementById('logout').submit();">Logout</a>
                 </div>
             </div>
         @else
@@ -47,7 +56,6 @@
     </div>
     <div class="sm:hidden absolute w-full z-10 bg-white border-t rounded-b-md shadow-lg" id="mobile-menu" x-show="menuOpen" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95">
         <div class="space-y-1 px-2 pt-2 pb-3">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="#" class="bg-palette-5 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
